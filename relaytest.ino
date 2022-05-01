2a39d4612cd899230f9e4e051cf7b57ab76a8169
Justin <76490882+justinmbaltazar@users.noreply.github.com>
2022-05-01 14:31:08 -0400

    Update relaytest.ino

--- relaytest.ino
+++ relaytest.ino
@@ -4,17 +4,17 @@
 //you can remove the pins as needed
 #define interval 1000 //milliseconds delay
 void setup() {
-  pinMode(relay1, OUTPUT);
+  pinMode(relay1, OUTPUT); //setup the relay
   pinMode(relay2, OUTPUT);
   pinMode(relay3, OUTPUT);
 }
 void loop()
 {
-   digitalWrite(relay1, HIGH);
+   digitalWrite(relay1, HIGH); //on
    digitalWrite(relay2, HIGH);
    digitalWrite(relay3, HIGH);
    delay(interval);
-   digitalWrite(relay1, LOW);
+   digitalWrite(relay1, LOW); //off
    digitalWrite(relay2, LOW);
    digitalWrite(relay3, LOW);
    delay(interval);
